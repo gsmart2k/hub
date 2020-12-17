31872261549da8497aab1d8b565f0fc13fdb62c7
gsmart2k <36375989+gsmart2k@users.noreply.github.com>
2020-12-17 11:04:44 +0100

    Delete users.js

--- login_system/SERVER/users.js
+++
@@ -1,7 +0,0 @@
-const users = [
-    {username : "ogunrinade", password: "Fatherwell2000", email:"ogunrinadegbolahan@gmail.com"},
-    {username : "Gbolahan", password: "Motherwell2000", email:"ogunrinadegbolahan200@gmail.com"}
-]
-
-
-module.exports = users
